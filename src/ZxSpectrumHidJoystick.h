13eadc6aac5b7d4db492a2e0848f38dcdaa8c649
Phil <pmscull@gmail.com>
2022-03-27 21:47:26 +0100

    Cache joystick decodes

--- src/ZxSpectrumHidJoystick.h
+++ src/ZxSpectrumHidJoystick.h
@@ -4,7 +4,9 @@
 #include "ZxSpectrumJoystick.h"
 
 class ZxSpectrumHidJoystick : public ZxSpectrumJoystick {
-  
+private:
+  uint32_t _updated;
+  uint8_t _kempston;
 public:
   virtual uint8_t __not_in_flash_func(kempston)();
 };
