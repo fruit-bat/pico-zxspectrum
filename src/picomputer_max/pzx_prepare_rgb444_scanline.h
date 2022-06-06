e67b839320179f8bd180c26c8668f4ec5fa3929b
Phil <pmscull@gmail.com>
2022-06-06 07:15:25 +0100

    Fast driver for st7789

--- src/picomputer_max/pzx_prepare_rgb444_scanline.h
+++ src/picomputer_max/pzx_prepare_rgb444_scanline.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "pico/stdlib.h"
+#include "hardware/pio.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,7 +17,18 @@ void __not_in_flash_func(pzx_prepare_rgb444_scanline)(
 );
 
 void __not_in_flash_func(pzx_send_rgb444_scanline)(
+  PIO pio,
+  uint sm,
   uint32_t y, 
+  uint32_t frame,
+  uint8_t* screenPtr,
+  uint8_t* attrPtr,
+  uint8_t borderColor
+);
+
+void __not_in_flash_func(pzx_send_rgb444_frame)(
+  PIO pio,
+  uint sm,
   uint32_t frame,
   uint8_t* screenPtr,
   uint8_t* attrPtr,
