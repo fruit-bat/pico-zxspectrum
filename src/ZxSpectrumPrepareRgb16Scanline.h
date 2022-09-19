8f121f794fc0d7d3c0c0c6999a0c2405c6e9f11b
Phil <pmscull@gmail.com>
2022-09-19 22:36:42 +0100

    prepare for Pimoroni Pico VGA

---
+++ src/ZxSpectrumPrepareRgb16Scanline.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "pico/stdlib.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __not_in_flash_func(zx_prepare_rgb16_scanline)(
+  uint32_t* buf, 
+  uint32_t y, 
+  uint32_t frame,
+  uint8_t* screenPtr,
+  uint8_t* attrPtr,
+  uint8_t borderColor
+);
+
+#ifdef __cplusplus
+}
+#endif
