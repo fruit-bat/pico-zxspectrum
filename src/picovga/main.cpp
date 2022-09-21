3e16fc3454367b3907a4099ca6551769e33a3453
Phil <pmscull@gmail.com>
2022-09-21 21:06:13 +0100

    prepare for Pimoroni Pico VGA

--- src/picovga/main.cpp
+++ src/picovga/main.cpp
@@ -26,7 +26,7 @@
 #include "ZxSpectrumDualJoystick.h"
 #include "ZxSpectrumHidJoystick.h"
 
-#include "bsp/board.h"
+//#include "bsp/board.h"
 #include "tusb.h"
 #include <pico/printf.h>
 #include "SdCardFatFsSpi.h"
@@ -40,7 +40,7 @@
 
 #define LED_PIN 25
 #define SPK_PIN 9
-#define vga_mode vga_mode_640x320_60
+#define VGA_MODE vga_mode_640x480_60
 
 #define VREG_VSEL VREG_VOLTAGE_1_10
 
@@ -139,9 +139,70 @@ unsigned char* screenPtr;
 unsigned char* attrPtr;
 static volatile uint _frames = 0;
 
+
+#define MIN_COLOR_RUN 3
+
+int32_t __not_in_flash_func(single_color_scanline)(uint32_t *buf, size_t buf_length, int width, uint32_t color16) {
+    assert(buf_length >= 2);
+
+    assert(width >= MIN_COLOR_RUN);
+    // | jmp color_run | color | count-3 |  buf[0] =
+    buf[0] = COMPOSABLE_COLOR_RUN | (color16 << 16);
+    buf[1] = (width - MIN_COLOR_RUN) | (COMPOSABLE_RAW_1P << 16);
+    // note we must end with a black pixel
+    buf[2] = 0 | (COMPOSABLE_EOL_ALIGN << 16);
+
+    return 3;
+}
+
+void __not_in_flash_func(render_scanline)(struct scanvideo_scanline_buffer *dest, int core) {
+    uint32_t *buf = dest->data;
+    size_t buf_length = dest->data_max;
+
+    int l = scanvideo_scanline_number(dest->scanline_id);
+    uint16_t bgcolour = (uint16_t) l << 2;
+    dest->data_used = single_color_scanline(buf, buf_length, VGA_MODE.width, bgcolour);
+    dest->status = SCANLINE_OK;
+}
+
+
+
+
 void __not_in_flash_func(core1_main)() {
   sem_acquire_blocking(&dvi_start_sem);
   printf("Core 1 running...\n");
+
+
+    int core_num = get_core_num();
+    printf("Rendering on core %d\n", core_num);
+    while (true) {
+        struct scanvideo_scanline_buffer *scanline_buffer = scanvideo_begin_scanline_generation(true);
+        uint32_t frame_num = scanvideo_frame_number(scanline_buffer->scanline_id);
+        uint32_t y = scanvideo_scanline_number(scanline_buffer->scanline_id);
+
+
+        render_scanline(scanline_buffer, core_num);
+
+        // Release the rendered buffer into the wild
+        scanvideo_end_scanline_generation(scanline_buffer);
+        
+        if (y == 239) { // TODO use a const / get from vmode
+          
+          // TODO Tidy this mechanism up
+          screenPtr = zxSpectrum.screenPtr();
+          attrPtr = screenPtr + (32 * 24 * 8);
+          
+          if (toggleMenu) {
+            showMenu = !showMenu;
+            toggleMenu = false;
+    //        picomputerJoystick.enabled(!showMenu);
+          }
+          
+          _frames = frame_num;
+        }
+    }
+
+
 
   // TODO fetch the resolution from the mode ?
   //VgaInit(vmode,640,480);
@@ -286,6 +347,9 @@ int main(){
           
   picoDisplay.refresh();
   
+  scanvideo_setup(&VGA_MODE);
+  scanvideo_timing_enable(true);
+
   sem_release(&dvi_start_sem);
  
   if (sdCard0.mount()) {
