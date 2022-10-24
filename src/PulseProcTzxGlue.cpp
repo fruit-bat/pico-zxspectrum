0fb27d3e41c83ca674e6292b0f2d9640a0ca154f
Phil <pmscull@gmail.com>
2022-10-24 21:07:06 +0100

    TZX random ideas

---
+++ src/PulseProcTzxGlue.cpp
@@ -0,0 +1,42 @@
+#include "PulseProcTzxGlue.h"
+#include <cstring>
+
+PulseProcTzxGlue::PulseProcTzxGlue() 
+{}
+  
+void PulseProcTzxGlue::init(PulseProc *nxt) {
+  next(nxt);
+}
+
+int32_t __not_in_flash_func(PulseProcTzxGlue::advance)(
+  InputStream *is,
+  bool *pstate,
+  PulseProc **top
+) {
+  uint8_t header[9];
+  
+  DBG_PULSE("About to read TZX glue\n");
+  int l = is->read(header, 9);
+
+  if (l < 9) {
+    DBG_PULSE("Could not read complete TZX glue\n");
+    return PP_ERROR;
+  }
+  
+  if (strncmp((const char*)header, "XTape!", 6) != 0) {
+    DBG_PULSE("Could not verify TZX glue bytes\n");
+    return PP_ERROR;
+  }
+  
+  if (header[6] != 0x1a) {
+    DBG_PULSE("Could not find end of text file marker\n");
+    return  PP_ERROR;
+  }
+  
+  uint8_t version_major = header[7];
+  uint8_t version_minor = header[8];
+  
+  DBG_PULSE("Found TZX glue version %d %d\n", version_major, version_minor);
+
+  return PP_CONTINUE;
+}
---
+++ src/PulseProcTzxHeader.cpp
@@ -0,0 +1,42 @@
+#include "PulseProcTzxHeader.h"
+#include <cstring>
+
+PulseProcTzxHeader::PulseProcTzxHeader() 
+{}
+  
+void PulseProcTzxHeader::init(PulseProc *nxt) {
+  next(nxt);
+}
+
+int32_t __not_in_flash_func(PulseProcTzxHeader::advance)(
+  InputStream *is,
+  bool *pstate,
+  PulseProc **top
+) {
+  uint8_t header[10];
+  
+  DBG_PULSE("About to read TZX header\n");
+  int l = is->read(header, 10);
+
+  if (l < 10) {
+    DBG_PULSE("Could not read complete TZX header\n");
+    return PP_ERROR;
+  }
+  
+  if (strncmp((const char*)header, "ZXTape!", 7) != 0) {
+    DBG_PULSE("Could not verify TZX signature bytes\n");
+    return PP_ERROR;
+  }
+  
+  if (header[7] != 0x1a) {
+    DBG_PULSE("Could not find end of text file marker\n");
+    return  PP_ERROR;
+  }
+  
+  uint8_t version_major = header[8];
+  uint8_t version_minor = header[9];
+  
+  DBG_PULSE("Found TZX header version %d %d\n", version_major, version_minor);
+
+  return PP_CONTINUE;
+}
