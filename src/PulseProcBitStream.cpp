9f6784cc01e075e5955e62db6d646eef63cb633b
Phil <pmscull@gmail.com>
2022-11-03 22:04:10 +0000

    TZX block 15 - Direct Recording

--- src/PulseProcBitStream.cpp
+++ src/PulseProcBitStream.cpp
@@ -25,6 +25,18 @@ int32_t __not_in_flash_func(PulseProcBitStream::advance)(
   if (_b & 0x10000) {
     int32_t r = is->readByte();
     if (r < 0) return PP_ERROR;
+    if (_l > 1 || _blb == 8) {
+      if (r == 0) {
+        *pstate = false;
+        --_l;
+        return _tspb << 3;
+      }
+      else if (r == 0xff) {
+        *pstate = true;
+        --_l;
+        return _tspb << 3;
+      }
+    }
     _b = r | (1 << (--_l ? 8 : 16 - _blb));
   }
   
