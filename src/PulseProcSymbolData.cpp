fda28324464cc0640839b285cac85f1e0ae72f89
Phil <pmscull@gmail.com>
2022-11-05 22:35:48 +0000

    TZX block 19 - Generalized Data Block

--- src/PulseProcSymbolData.cpp
+++ src/PulseProcSymbolData.cpp
@@ -1,5 +1,6 @@
 #include "PulseProcSymbolData.h"
 
+
 int32_t __not_in_flash_func(PulseProcSymbolData::advance)(
   InputStream *is,
   bool *pstate,
@@ -13,12 +14,42 @@ int32_t __not_in_flash_func(PulseProcSymbolData::advance)(
     int32_t r = is->readByte();
     DBG_PULSE("PulseProcSymbolData: data %ld\n", r);      
     if (r < 0) return PP_ERROR;    
-    _sd = r | (_sd << (8 - _sb));
+    _sd = r | (_sd << 8);
     _sb += 8;
   }
-  uint32_t si = _sm & (_sd >> (_sb-- - _bps));
+  _sb -= _bps;
+  uint32_t si = _sm & (_sd >> _sb);
   --_ns;
    _symbol->init(this, si);
   *top = _symbol;
   return PP_CONTINUE;
 }
+
+/*
+
+int32_t __not_in_flash_func(PulseProcSymbolData::advance)(
+  InputStream *is,
+  bool *pstate,
+  PulseProc **top
+) {
+  if (_ns == 0)  {
+    *pstate = !*pstate;
+    return PP_COMPLETE;
+  }
+  if (_sb < _bps)  {
+    int32_t r = is->readByte();
+    DBG_PULSE("PulseProcSymbolData: data %ld\n", r);      
+    if (r < 0) return PP_ERROR;    
+    _sd |= r << _sb;
+    _sb += 8;
+  }
+  uint32_t si = _sm & _sd;
+  _sd >>= _bps;
+  _sb -= _bps;
+  --_ns;
+  _symbol->init(this, si);
+  *top = _symbol;
+  return PP_CONTINUE;
+}
+
+*/
