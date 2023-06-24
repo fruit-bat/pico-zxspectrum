5fa4d009eb642aded6fa7c019ea6ebfd9762e12a
Phil <pmscull@gmail.com>
2023-06-24 09:36:58 +0100

    Persistent settings

--- src/ZxSpectrumSettings.cpp
+++ src/ZxSpectrumSettings.cpp
@@ -1,9 +1,19 @@
 #include "ZxSpectrumSettings.h"
 
- ZxSpectrumSettings::ZxSpectrumSettings() {
+ZxSpectrumSettings::ZxSpectrumSettings()
+{
+}
 
- }
+ZxSpectrumSettings::~ZxSpectrumSettings()
+{
+}
 
-ZxSpectrumSettings::~ZxSpectrumSettings() {
+bool ZxSpectrumSettings::save(ZxSpectrumSettingValues *values)
+{
+    return false;
+}
 
-} 
+bool ZxSpectrumSettings::load(ZxSpectrumSettingValues *values)
+{
+    return false;
+}
--- src/ZxSpectrumSettings.h
+++ src/ZxSpectrumSettings.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <stdlib.h>
+#include "ZxSpectrumJoystick.h"
+
+typedef struct {
+  uint32_t volume;
+  ZxSpectrumJoystickMode mode;
+} ZxSpectrumSettingValues;
 
 class ZxSpectrumSettings {
 
 public:
   ZxSpectrumSettings();
   virtual ~ZxSpectrumSettings();
+
+  virtual bool save(ZxSpectrumSettingValues *values);
+  virtual bool load(ZxSpectrumSettingValues *values);
 };
 
