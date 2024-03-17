7aeee66a209a18801fce26777b8858a0d32e2d82
Phil <pmscull@gmail.com>
2024-03-17 07:15:04 +0000

    HDMI audio

--- src/hdmi/main.cpp
+++ src/hdmi/main.cpp
@@ -177,43 +177,72 @@ unsigned char* screenPtr;
 unsigned char* attrPtr;
 static volatile uint _frames = 0;
 
-void __not_in_flash_func(core1_scanline_callback)() {
-  static uint y = 1;
+void __not_in_flash_func(core1_render)() {
+  static uint y = 0;
   static uint ys = 0;
-  if (y == 0) _frames++;
-  if (showMenu) {
-    uint rs = pcw_prepare_scanline_80(&dvi0, y++, ys, _frames);
-    if (0 == (y & 7)) {
-      ys += rs;
+  while(true) {
+    if (showMenu) {
+      uint rs = pcw_prepare_scanline_80(&dvi0, y++, ys, _frames);
+      if (0 == (y & 7)) {
+        ys += rs;
+      }
     }
-  }
-  else {
-    zx_prepare_hdmi_scanline(&dvi0, y++, _frames, screenPtr, attrPtr, zxSpectrum.borderColour());
-  }
-#ifdef USE_KEY_MATRIX
-  zx_keyscan_row();
-#endif
-  if (y == FRAME_HEIGHT) {
-    y = 0;
-    ys = 0;
-    
-    // TODO Tidy this mechanism up
-    screenPtr = zxSpectrum.screenPtr();
-    attrPtr = screenPtr + (32 * 24 * 8);
-    
-    if (toggleMenu) {
-      showMenu = !showMenu;
-      toggleMenu = false;
+    else {
+      zx_prepare_hdmi_scanline(&dvi0, y++, _frames, screenPtr, attrPtr, zxSpectrum.borderColour());
+    }
+  #ifdef USE_KEY_MATRIX
+  // zx_keyscan_row();
+  #endif
+    if (y == FRAME_HEIGHT) {
+      y = 0;
+      ys = 0;
+      _frames++;
+      // TODO Tidy this mechanism up
+      screenPtr = zxSpectrum.screenPtr();
+      attrPtr = screenPtr + (32 * 24 * 8);
+      
+      if (toggleMenu) {
+        showMenu = !showMenu;
+        toggleMenu = false;
+      }
     }
   }
 }
 
+//Audio Related
+#define AUDIO_BUFFER_SIZE   256
+audio_sample_t      audio_buffer[AUDIO_BUFFER_SIZE];
+struct repeating_timer audio_timer;
+const int16_t sine[32] = {
+    (short)0x8000,(short)0x98f8,(short)0xb0fb,(short)0xc71c,(short)0xda82,(short)0xea6d,(short)0xf641,(short)0xfd89,
+    (short)0xffff,(short)0xfd89,(short)0xf641,(short)0xea6d,(short)0xda82,(short)0xc71c,(short)0xb0fb,(short)0x98f8,
+    (short)0x8000,(short)0x6707,(short)0x4f04,(short)0x38e3,(short)0x257d,(short)0x1592,(short)0x9be,(short)0x276,
+    (short)0x0,(short)0x276,(short)0x9be,(short)0x1592,(short)0x257d,(short)0x38e3,(short)0x4f04,(short)0x6707
+};
+
+bool __not_in_flash_func(audio_timer_callback)(struct repeating_timer *t) {
+    int size = get_write_size(&dvi0.audio_ring, false);
+    audio_sample_t *audio_ptr = get_write_pointer(&dvi0.audio_ring);
+    audio_sample_t sample;
+    static uint sample_count = 0;
+    for (int cnt = 0; cnt < size; cnt++) {
+        sample.channels[0] = sine[sample_count % 32] /8;
+        sample.channels[1] = sine[sample_count % 32] /8;
+        *audio_ptr++ = sample;
+        sample_count++;
+    }
+    increase_write_pointer(&dvi0.audio_ring, size);
+ 
+    return true;
+}
+
 void __not_in_flash_func(core1_main)() {
+  
   dvi_register_irqs_this_core(&dvi0, DMA_IRQ_1);
   sem_acquire_blocking(&dvi_start_sem);
-
   dvi_start(&dvi0);
 
+core1_render();
   // The text display is completely IRQ driven (takes up around 30% of cycles @
   // VGA). We could do something useful, or we could just take a nice nap
   while (1) 
@@ -337,12 +366,14 @@ int main() {
   printf("Configuring DVI\n");
   dvi0.timing = &DVI_TIMING;
   dvi0.ser_cfg = DVI_DEFAULT_SERIAL_CONFIG;
-  dvi0.scanline_callback = core1_scanline_callback;
   dvi_init(&dvi0, next_striped_spin_lock_num(), next_striped_spin_lock_num());
 
-  printf("Prepare first scanline\n");
-
-  zx_prepare_hdmi_scanline(&dvi0, 0, _frames, screenPtr, attrPtr, zxSpectrum.borderColour());
+  // HDMI Audio related
+  dvi_get_blank_settings(&dvi0)->top    = 0;
+  dvi_get_blank_settings(&dvi0)->bottom = 0;
+  dvi_audio_sample_buffer_set(&dvi0, audio_buffer, AUDIO_BUFFER_SIZE);
+  dvi_set_audio_freq(&dvi0, 44100, 28000, 6272);
+  add_repeating_timer_ms(2, audio_timer_callback, NULL, &audio_timer);
 
   printf("Core 1 start\n");
   sem_init(&dvi_start_sem, 0, 1);
