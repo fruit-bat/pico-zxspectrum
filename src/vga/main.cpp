f3cb595785e472dae76f2398fb094bfc90028e73
javavi <javavict@gmail.com>
2024-06-03 22:51:12 +0300

    PS/2 Kbd MURMULATOR Fix

--- src/vga/main.cpp
+++ src/vga/main.cpp
@@ -122,7 +122,7 @@ extern "C"  void __not_in_flash_func(process_kbd_report)(hid_keyboard_report_t c
 #ifdef USE_MRMLTR_PS2_KBD
 static Ps2Kbd_Mrmltr ps2kbd(
   pio1,
-  8,
+  0,
   process_kbd_report
 );
 #elif defined(USE_PS2_KBD)
