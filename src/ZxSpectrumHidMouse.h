2d4911bf4d2740c35bd150173aa2dde593be26b6
Phil <pmscull@gmail.com>
2024-06-28 13:58:49 +0100

    mouse

---
+++ src/ZxSpectrumHidMouse.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <pico/stdlib.h>
+#include "ZxSpectrumMouse.h"
+
+class ZxSpectrumHidMouse : ZxSpectrumMouse {
+private:
+public:
+  ZxSpectrumHidMouse() {}
+};
---
+++ src/ZxSpectrumMouse.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <pico/stdlib.h>
+
+
+class ZxSpectrumMouse {
+private:
+public:
+  ZxSpectrumMouse() {}
+};
