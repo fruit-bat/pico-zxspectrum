5047ada5c20d4fc5acec0856ef84e0262e8b0ba7
dprcz <pribyl.email@gmail.com>
2024-07-29 21:16:42 +0200

    feature/menu_controll_by_joystick

--- src/picomputer/picomputer_vga_zx/main.cpp
+++ src/picomputer/picomputer_vga_zx/main.cpp
@@ -159,7 +159,7 @@ void __not_in_flash_func(process_picomputer_kbd_report)(hid_keyboard_report_t co
   }
   else {
     r = keyboard2.processHidReport(report, prev_report);
-    s = hidJoystick.testbt4press();
+    s = hidJoystick.tstjoy1bt3();
   }
   if (r | s) {
     toggleMenu = true;
