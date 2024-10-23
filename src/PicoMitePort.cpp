62c4642783beddaa54cded1f5eca418e88d2b986
Phil <pmscull@gmail.com>
2024-10-23 21:25:01 +0100

    IO port

---
+++ src/PicoMitePort.cpp
@@ -0,0 +1,14 @@
+#include "PicoMitePort.h"
+
+void PicoMitePort::init() {
+
+}
+
+
+uint8_t __not_in_flash_func(PicoMitePort::read)() {
+    return 0;
+}
+
+void __not_in_flash_func(PicoMitePort::write)(uint8_t data) {
+
+}
--- src/ZxSpectrumPort.h
+++ src/ZxSpectrumPort.h
@@ -5,4 +5,5 @@ class ZxSpectrumPort {
 public:
   virtual uint8_t __not_in_flash_func(read)() = 0;
   virtual void __not_in_flash_func(write)(uint8_t data) = 0;
+  virtual void init();
 };
