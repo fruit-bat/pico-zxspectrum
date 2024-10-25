13559a1c7eae57b16a1c26a2da87181ccea5f162
Phil <pmscull@gmail.com>
2024-10-25 20:00:05 +0100

    IO port

--- src/PicoMitePort.cpp
+++ src/PicoMitePort.cpp
@@ -1,14 +1,32 @@
 #include "PicoMitePort.h"
+#include "hardware/adc.h"
+
+// Output pins on GP10, GP11, GP12, GP13
+static const uint8_t POUT_PINS[] = {10, 11, 12, 13};
 
 void PicoMitePort::init() {
+    adc_init();
 
+    // Make sure GPIO is high-impedance, no pullups etc
+    adc_gpio_init(26);
+
+    // Select ADC input 2 (GPIO28)
+    adc_select_input(2);
+
+    for(size_t i = 0; i < sizeof(POUT_PINS); ++i) {
+        gpio_init(POUT_PINS[i]);
+        gpio_set_dir(POUT_PINS[i], GPIO_OUT);
+    }
 }
 
-
 uint8_t __not_in_flash_func(PicoMitePort::read)() {
-    return 0;
+    uint16_t result = adc_read(); // Read 12 bit ADC
+    return (result >> 4) & 0xff;
 }
 
 void __not_in_flash_func(PicoMitePort::write)(uint8_t data) {
-
+    for(size_t i = 0; i < sizeof(POUT_PINS); ++i) {
+        gpio_put(POUT_PINS[i], (data & 1) == 1);
+        data >>= 1;
+    }
 }
