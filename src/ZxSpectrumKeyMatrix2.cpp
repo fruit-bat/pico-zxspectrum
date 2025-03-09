e34a4553e79e9faf591532b7bc3c6f4795f44de1
Phil <pmscull@gmail.com>
2025-03-09 10:25:52 +0000

    Bobricus PicoZero PICO ZX

--- src/ZxSpectrumKeyMatrix2.cpp
+++ src/ZxSpectrumKeyMatrix2.cpp
@@ -41,22 +41,19 @@ static uint8_t kbi = 0;
 
 static uint8_t kbits[1][COL_PIN_COUNT][ROW_PIN_COUNT] = {
   {
-    { HID_KEY_1,     HID_KEY_2, HID_KEY_3, HID_KEY_4, HID_KEY_5, HID_KEY_F10 /* 128 K */ },
-    { HID_KEY_Q,     HID_KEY_W, HID_KEY_E, HID_KEY_R, HID_KEY_T, HID_KEY_F9  /* 48 K */ },
-    { HID_KEY_A,     HID_KEY_S, HID_KEY_D, HID_KEY_F, HID_KEY_G, HID_KEY_F1  /* Menu */ },
-    { HID_KEY_0,     HID_KEY_9, HID_KEY_8, HID_KEY_7, HID_KEY_6, HID_KEY_ARROW_DOWN /* Down */ },
-    { HID_KEY_P,     HID_KEY_O, HID_KEY_I, HID_KEY_U, HID_KEY_Y, HID_KEY_ARROW_RIGHT /* Right */ },
-    { 0,/* Shift */  HID_KEY_Z, HID_KEY_X, HID_KEY_C, HID_KEY_Z, HID_KEY_ARROW_UP /* Up */ },
-    { HID_KEY_ENTER, HID_KEY_L, HID_KEY_K, HID_KEY_J, HID_KEY_H, HID_KEY_BRACKET_LEFT /* Left */ },
-    { HID_KEY_SPACE, HID_KEY_ALT_RIGHT, HID_KEY_M, HID_KEY_N, HID_KEY_B, 0 /* Fire */ }
+/* 0 */ { HID_KEY_1,     HID_KEY_2,         HID_KEY_3, HID_KEY_4, HID_KEY_5, HID_KEY_F10          /* 128 K */ },
+/* 1 */ { HID_KEY_Q,     HID_KEY_W,         HID_KEY_E, HID_KEY_R, HID_KEY_T, HID_KEY_F9           /* 48 K  */ },
+/* 2 */ { HID_KEY_A,     HID_KEY_S,         HID_KEY_D, HID_KEY_F, HID_KEY_G, HID_KEY_F1           /* Menu  */ },
+/* 3 */ { HID_KEY_0,     HID_KEY_9,         HID_KEY_8, HID_KEY_7, HID_KEY_6, HID_KEY_ARROW_DOWN   /* Down  */ },
+/* 4 */ { HID_KEY_P,     HID_KEY_O,         HID_KEY_I, HID_KEY_U, HID_KEY_Y, HID_KEY_ARROW_RIGHT  /* Right */ },
+/* 5 */ { 0,/* Shift */  HID_KEY_Z,         HID_KEY_X, HID_KEY_C, HID_KEY_Z, HID_KEY_ARROW_UP     /* Up    */ },
+/* 6 */ { HID_KEY_ENTER, HID_KEY_L,         HID_KEY_K, HID_KEY_J, HID_KEY_H, HID_KEY_BRACKET_LEFT /* Left  */ },
+/* 7 */ { HID_KEY_SPACE, HID_KEY_ALT_RIGHT, HID_KEY_M, HID_KEY_N, HID_KEY_B, 0                    /* Fire  */ }
   }
 };
 
-// TODO Wrong
-#define KEY_SHIFT_ROW 0
-#define KEY_SHIFT_BIT 0x20
-#define KEY_CTRL_ROW 5
-#define KEY_CTRL_BIT 0x01
+#define KEY_SHIFT_ROW 5
+#define KEY_SHIFT_BIT 0x1
 #define LED_PIN 25
 
 void zx_keyscan_init() {
@@ -89,8 +86,8 @@ void zx_keyscan_init() {
 }
 
 void __not_in_flash_func(zx_keyscan_row)() {
-  static uint32_t ri = 0;
-  static uint32_t si = 0;
+  static uint32_t ri = 0; /* The column index */
+  static uint32_t si = 0; /* The sample index */
 
   gpio_put(COL_PIN_CLK, 0);
 
@@ -126,14 +123,14 @@ void __not_in_flash_func(zx_keyscan_get_hid_reports)(hid_keyboard_report_t const
   static uint8_t modifier = 0;
   
   bool shift = rdb[KEY_SHIFT_ROW] & KEY_SHIFT_BIT;
-  bool ctrl = rdb[KEY_CTRL_ROW] & KEY_CTRL_BIT;
+  //bool ctrl = rdb[KEY_CTRL_ROW] & KEY_CTRL_BIT;
 
   // Build the current hid report
   uint32_t ki = 0;
   hid_keyboard_report_t *chr = &hr[hri & 1];
   chr->modifier = modifier;
   if (shift) chr->modifier |= 2;
-  if (ctrl) chr->modifier |= 1;
+  //if (ctrl) chr->modifier |= 1;
   for(int ri = 0; ri < COL_PIN_COUNT; ++ri) {
     uint8_t r = rdb[ri];
     uint32_t ci = 0;
